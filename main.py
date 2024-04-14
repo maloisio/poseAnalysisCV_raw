1eb15464a9fec696e269e0c5407500f5d98c43a7
lucasarias <lucasarias@alunos.utfpr.edu.br>
2024-04-14 16:18:39 -0300

    Já verifica se esta reto ou não, falta verificar os estados nessa versão

--- main.py
+++ main.py
@@ -35,26 +35,47 @@ EDGES = {
 def mostra_posicoes(keypoints):
     for linha in keypoints_with_scores[0]:
             print("olhos",[linha[0]])
-            print("ombro esquerdo",[linha[5]])
+            # print("ombro esquerdo",[linha[5]])
             print("ombro direito",[linha[6]])
-            print("cotovelo esquerdo",[linha[7]])
+            # print("cotovelo esquerdo",[linha[7]])
             print("cotovelo direito",[linha[8]])
-            print("mao esquerdo",[linha[9]])
-            print("mao direito",[linha[10]])
+            # print("mao esquerdo",[linha[9]])
+            # print("mao direito",[linha[10]])
             print("quadril esquerdo",[linha[11]])
             print("quadril direito",[linha[12]])
-            print("joelho esquerdo",[linha[13]])
-            print("joelho direito",[linha[14]])
-            print("pe esquerdo",[linha[15]])
-            print("pe direito",[linha[16]])
+            # print("joelho esquerdo",[linha[13]])
+            # print("joelho direito",[linha[14]])
+            # print("pe esquerdo",[linha[15]])
+            # print("pe direito",[linha[16]])
 
+
+def erro_de_10(valor1,valor2):
+    if(valor1 > valor2):
+        if(valor1 < valor2 * 1.15):
+            return True
+        else:
+            return False
+    else:
+        if(valor1*1.15 > valor2):
+            return True
+        else:
+            return False
+        
+
+def verificar_reto(keypoints):
+    for linha in keypoints_with_scores[0]:
+        if(erro_de_10(float(linha[12][1])/float(linha[12][0]),float(linha[6][1])/float(linha[6][0])) and erro_de_10(float(linha[11][1])/float(linha[11][0]),float(linha[5][1])/float(linha[5][0]))):
+            return True
+        else:
+            return False
+        
 
 def check_flexao(keypoints, tempo, contflec,contelap):
         #print(tempo,contelap)
         if (tempo > contelap + 20):
             for linha in keypoints_with_scores[0]:
-                if(linha[5][0] > linha[7][0]) and (linha[6][0] > linha[8][0]) and (linha[0][0] > linha[5][0]): #talvez uma lógica para ver se está tendo flexão
-                    print("flexionado")
+                if(linha[12][0] > linha[8][0]) and (linha[11][0] > linha[6][0]) : #talvez uma lógica para ver se está tendo flexão
+                    print(float(linha[6][1]))
                     contflec = contflec + 1
                     contelap = tempo
                     
@@ -87,12 +108,13 @@ contagem_de_vezes = 0
 limitador_de_frames = int(input("Defina limitador de quantidade total de frames:"))
 contador_De_flexoes = contagem_de_vezes
 contador_de_tempo_elapsado = contagem_de_vezes
+res = (0,0)
 while cap.isOpened():
     ret, frame = cap.read()
     frame = cv.resize(frame, [480, 480], interpolation=cv.INTER_BITS)
     #frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
     #frame = cv.rotate(frame, cv.ROTATE_180);
-
+    
     frame2 = np.zeros((frame.shape[0], frame.shape[1], 3), np.uint8)  # criacao imagem preta
 
     #reshape imagem para 192x192x3
@@ -108,10 +130,15 @@ while cap.isOpened():
     interpreter.set_tensor(input_details[0]['index'], np.array(input_image))
     interpreter.invoke()
     keypoints_with_scores = interpreter.get_tensor(output_details[0]['index'])
-    if contagem_de_vezes%10 == 0: #A cada 10 frames me printa a posição de cada coisa
-        mostra_posicoes(keypoints_with_scores)
+    # if contagem_de_vezes%10 == 0: #A cada 10 frames me printa a posição de cada coisa
+    #     mostra_posicoes(keypoints_with_scores)
             
-    res = check_flexao(keypoints_with_scores, contagem_de_vezes, contador_De_flexoes, contador_de_tempo_elapsado)   
+    if(verificar_reto(keypoints_with_scores)):
+        print("reto")
+        res = check_flexao(keypoints_with_scores, contagem_de_vezes, contador_De_flexoes, contador_de_tempo_elapsado)   
+    else:
+        print("Não está reto")
+    
     contador_De_flexoes = res[0]
     contador_de_tempo_elapsado = res[1]
     contagem_de_vezes = contagem_de_vezes + 1
